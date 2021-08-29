1bc908f8b9fb584950891d56244b096f0c716b5b
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-29 23:39:40 +0600

    Dynamic data types

---
+++ data-types.js
@@ -0,0 +1,10 @@
+//constant data type
+/* const studentId = 181;
+const studentName = 'Rashed';
+console.log(studentName);
+ */
+
+// let data type
+let studentId = 15;
+let studentName = 'Alam';
+console.log('Student Name :', typeof studentName, ' ', 'Student Id :', typeof studentId);
