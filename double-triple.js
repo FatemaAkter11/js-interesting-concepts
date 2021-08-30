d08cbcd709b08f976de9b7fa03535124d705e867
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-30 11:21:10 +0600

    double equal (==) vs triple equal (===)

---
+++ double-triple.js
@@ -0,0 +1,30 @@
+//Double Equal
+const first = '0';
+const second = false;
+if (first == second) {
+    // console.log('Condition is true');
+}
+else {
+    //console.log('Condition is false');
+}
+//Triple Equal
+const a = 2;
+const b = '2';
+if (a === b) {
+    //  console.log('Condition is true');
+}
+else {
+    // console.log('Condition is false');
+}
+
+//More conversion
+//const x = { name: 'Ali' };
+//const y = { name: 'Ali' };
+const x = [];
+const y = [];
+if (x == y) {
+    console.log('Both are same');
+}
+else {
+    console.log('They are not same');
+}
